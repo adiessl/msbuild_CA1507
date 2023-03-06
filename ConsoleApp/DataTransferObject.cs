3a84165a651d01747a7afa02c4dd981cfd7d810d
Alexander Diessl <a.diessl@speedikonfm.com>
2023-03-06 11:46:24 +0100

    Add sample console app

---
+++ ConsoleApp/DataTransferObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace ConsoleApp;
+
+internal sealed class DataTransferObject
+{
+    [JsonProperty("Test")]
+    public string? Test { get; set; }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp;
+
+internal sealed class Program
+{
+    public static void Main(string[] args)
+    {
+        var dto = new DataTransferObject();
+    }
+}
